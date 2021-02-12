499827227709447cc6d8ae52ed5298affdb1dbb1
IgorLima <clockwise.info@gmail.com>
2021-02-12 14:09:39 +0000

    new commit

--- navbar.js
+++ navbar.js
@@ -1,22 +1,12 @@
 "use strict"
 const myNav= document.querySelector('nav');
-<<<<<<< HEAD
-
-addEventListener('scroll', () =>{
-
-=======
  
 addEventListener('scroll', () =>{
  
->>>>>>> main
     if(scrollY>400){
         myNav.style.setProperty('background',"blue");
     }
   else {
         myNav.style.setProperty('background',"transparent");
   }
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> main
